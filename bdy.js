27f21cacfdedb2f9f806a7fb33525bb7be952ac9
Anirudh gagneja <40909101+CDS02@users.noreply.github.com>
2020-10-15 06:14:08 +0530

    Add files via upload

---
+++ bdy.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function bdy() {
+  return (
+    <div className="App">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Reactkjhihoijlhiioj;ijlijijoijoijoioj
+        </a>
+    </div>
+  );
+}
+
+export default bdy;
